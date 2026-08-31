3e30f00d9e4cee93da2a4714a8d84f1b76ae2fae
KateTseng <KateTseng.dev@gmail.com>
2026-08-31 16:44:23 +0700

    refactor: remove custom exception layer

--- maaplus/errors.py
+++
@@ -1,6 +0,0 @@
-class MaaPlusError(Exception):
-    """Base exception for MaaPlus."""
-
-
-class RuntimeOperationError(MaaPlusError):
-    """Raised when a MaaFramework-backed runtime operation fails."""
