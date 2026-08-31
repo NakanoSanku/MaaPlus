f67c26181c7bef2b9a75f6ffe42ee1c590c0e2db
KateTseng <KateTseng.dev@gmail.com>
2026-08-31 21:30:40 +0700

    refactor: expose scheduler task api

--- maaplus/__init__.py
+++ maaplus/__init__.py
@@ -3,15 +3,16 @@ from maa.pipeline import JRecognitionParam as Locator
 from maa.pipeline import JRect as Rect
 from maa.pipeline import JTemplateMatch as Template
 
-from .runner import Runner
 from .runtime import MatchResult, Runtime
+from .scheduler import Scheduler, Task
 
 __all__ = [
     "Locator",
     "MatchResult",
     "OCR",
     "Rect",
-    "Runner",
     "Runtime",
+    "Scheduler",
+    "Task",
     "Template",
 ]
