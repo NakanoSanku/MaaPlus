a66d61e30d0dfeca25b96726503c93d6e9fba57f
KateTseng <KateTseng.dev@gmail.com>
2026-09-01 13:41:31 +0700

    feat: export routed flow helpers

--- maaplus/__init__.py
+++ maaplus/__init__.py
@@ -3,6 +3,7 @@ from maa.pipeline import JRecognitionParam as Locator
 from maa.pipeline import JRect as Rect
 from maa.pipeline import JTemplateMatch as Template
 
+from .routing import Navigator, RoutedFlow, routed
 from .runtime import MatchResult, Runtime
 from .scheduler import FlowResult, Scheduler, Task
 
@@ -10,10 +11,13 @@ __all__ = [
     "FlowResult",
     "Locator",
     "MatchResult",
+    "Navigator",
     "OCR",
     "Rect",
+    "RoutedFlow",
     "Runtime",
     "Scheduler",
     "Task",
     "Template",
+    "routed",
 ]
