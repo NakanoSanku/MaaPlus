3b2f0d7faea00b6a83c9b8d9c9abd1c596deea17
KateTseng <KateTseng.dev@gmail.com>
2026-09-02 09:43:25 +0700

    feat: export interaction strategies

--- maaplus/__init__.py
+++ maaplus/__init__.py
@@ -3,12 +3,15 @@ from maa.pipeline import JRecognitionParam as Locator
 from maa.pipeline import JRect as Rect
 from maa.pipeline import JTemplateMatch as Template
 
+from . import click, swipe, timing
 from .app import App, TaskHandle
+from .interaction import ClickConfig, InteractionConfig, SwipeConfig
 from .routing import Navigator, RoutedTaskHandler, routed
 from .runtime import MatchResult, Runtime
 from .scheduler import Scheduler
 from .task import Task, TaskHandler, TaskResult
 from .tick import Tick
+from .timing import Timing, TimingResolver
 
 CONTINUE = TaskResult.CONTINUE
 YIELD = TaskResult.YIELD
@@ -16,8 +19,10 @@ DONE = TaskResult.DONE
 
 __all__ = [
     "App",
+    "ClickConfig",
     "CONTINUE",
     "DONE",
+    "InteractionConfig",
     "Locator",
     "MatchResult",
     "Navigator",
@@ -26,12 +31,18 @@ __all__ = [
     "RoutedTaskHandler",
     "Runtime",
     "Scheduler",
+    "SwipeConfig",
     "Task",
     "TaskHandle",
     "TaskHandler",
     "TaskResult",
     "Template",
     "Tick",
+    "Timing",
+    "TimingResolver",
     "YIELD",
+    "click",
     "routed",
+    "swipe",
+    "timing",
 ]
